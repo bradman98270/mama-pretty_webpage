8c83abebd82b046966d1ad3c06c98bea88f7cf6d
Jun Alvarado <jun3rd@gmail.com>
2018-04-05 19:18:07 -0700

    placed files into industry suggested standard directory structure

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// document.getElementById("pricing").disabled = true;
+// document.getElementById("features").disabled = true;
+// document.getElementById("purchase").disabled = true;
+// document.getElementById("contactUs").disabled = true;
+
